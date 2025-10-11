002a0078fc2a311467832f515333609d77b6f29b
Bradford Loesch <bradford.loesch@gmail.com>
2025-10-11 12:03:57 -0400

    work in progress

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Neuro Inclusive Speech Therapy, LLC";
+export const SITE_DESCRIPTION = "Neuro Inclusive Speech Therapy, LLC: Offering pediatric and adult services in your home or via telehealth.";
+export const RESEND_API_KEY="re_j4Rscj6g_9K5gGDP6Yb9fgrn2aFgAJbGz"
+export const SEND_EMAIL_FROM="support@neuroinclusivetherapy.com"
