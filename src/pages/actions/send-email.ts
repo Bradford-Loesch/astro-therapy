e53f1eeee6db2067d7445c1b95036a2dfdb40b7f
Bradford Loesch <bradford.loesch@gmail.com>
2025-11-02 11:23:03 -0500

    cleanup, comment out form for now

--- src/pages/actions/send-email.ts
+++ src/pages/actions/send-email.ts
@@ -5,20 +5,23 @@ export const prerender = false;
 
 export const POST: APIRoute = async ({ request, redirect }) => {
   // Get the form data submitted by the user on the home page
+  const from = "contact@neuroinclusivespeech.com";
   const formData = await request.formData();
-  const from = formData.get("from") as string | null;
-  const subject = formData.get("subject") as string | null;
+  const email = formData.get("from") as string | null;
+  const phone = formData.get("phone") as string | null;
   const message = formData.get("message") as string | null;
 
   // Throw an error if we're missing any of the needed fields.
-  if (!from || !subject || !message) {
+  if (!email || !message) {
     throw new Error("Missing required fields");
   }
 
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (from && !emailRegex.test(from)) {
+  if (email && !emailRegex.test(email)) {
     throw new Error("Invalid email address");
   }
+
+  const subject = `New contact form submission from ${email}${phone ? ` | Phone: ${phone}` : ""}`;
 
   // Try to send the email using a `sendEmail` function we'll create next. Throw
   // an error if it fails.
